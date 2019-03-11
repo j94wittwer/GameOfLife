545a5b83396ea5f560f561491406ea54433e96ae
Jonas Wittwer <j94wittwer@gmail.com>
2019-03-11 14:58:57 +0100

    added functionality to determine Cell status

--- src/main/java/MVCModel/Cell.java
+++ src/main/java/MVCModel/Cell.java
@@ -1,5 +1,7 @@
 package MVCModel;
 
+import java.util.Random;
+
 public class Cell {
 
     private boolean isAlive;
@@ -7,12 +9,20 @@ public class Cell {
     private int yCoordinate;
 
     public Cell(int yCoordinate, int xCoordinate) {
-        this.isAlive = false;
+        this.isAlive = randomStatus();
         this.xCoordinate = xCoordinate;
         this.yCoordinate = yCoordinate;
     }
 
 
+    private boolean randomStatus(){
+        Random r = new Random();
+        if (r.nextInt(10) == 9){
+            return true;
+        }else {
+            return false;
+        }
+    }
 
     public boolean isAlive() {
         return isAlive;
